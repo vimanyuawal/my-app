dc792d6e682bee3f456ad8d219886621bd91aac7
Vimanyu Awal <awal@usc.edu>
2020-04-18 16:12:17 -0700

    Updates

---
+++ precache-manifest.1f631465670e927789bfa27da03b00a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe697d10de1ea65b39af77f91e4bebc",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "777866403fd482cbfc72",
+    "url": "/my-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "e3b877d97446e1284e75",
+    "url": "/my-app/static/css/main.26acc523.chunk.css"
+  },
+  {
+    "revision": "777866403fd482cbfc72",
+    "url": "/my-app/static/js/2.053e9936.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/my-app/static/js/2.053e9936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b877d97446e1284e75",
+    "url": "/my-app/static/js/main.d280759d.chunk.js"
+  },
+  {
+    "revision": "7017a0a3783b2a9ce739",
+    "url": "/my-app/static/js/runtime-main.b51f6de3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
